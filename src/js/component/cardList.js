9c7fcb92a3bab522a950b3aed64632b353afc26f
Jnv821 <jnv821@gmail.com>
2023-01-16 11:44:42 +0000

    Updated the code to fit another way to do it.

--- src/js/component/cardList.js
+++ src/js/component/cardList.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Row, Container } from 'react-bootstrap';
 import { CardComponent } from './card.js';
 
 export const CardList = (props) => {
 
-
     const cards = props.data.map((element, i) => {
-        return(<CardComponent key={i} data={props.data[i]} type={props.title}/>)
+        return(<CardComponent key={i} data={props.data[i]} type={props.title} info={props.info[i]}/>)
     })
 
     return (
         <Container>
             <h1>{props.title}</h1>
             <Row className='cardList'>
-                {cards}
+                {props.info.length ? cards : <h1>Loading</h1>}
             </Row>
         </Container>
     )
